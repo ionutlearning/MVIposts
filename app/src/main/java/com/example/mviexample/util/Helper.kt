25a2204c8ccb10fa6d0df2ca2280a48737b762ef
Ionut <faurionnut@gmail.com>
2020-06-19 16:04:39 +0300

    glide fix + images

--- app/src/main/java/com/example/mviexample/util/Helper.kt
+++ app/src/main/java/com/example/mviexample/util/Helper.kt
@@ -5,18 +5,29 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
 import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
-fun loadImage(view: ImageView, url: String?) {
+fun loadImage(view: ImageView, url: String) {
     val currentDrawable = view.drawable
     val options = RequestOptions()
         .placeholder(getProgressDrawable(view.context))
         .error(currentDrawable)
     Glide.with(view.context)
         .setDefaultRequestOptions(options)
-        .load(url)
+        .load(getUrl(url))
         .into(view)
+}
+
+private fun getUrl(url: String): GlideUrl {
+    return GlideUrl(
+        url,
+        LazyHeaders.Builder()
+            .addHeader("User-Agent", "https")
+            .build()
+    )
 }
 
 private fun getProgressDrawable(context: Context): CircularProgressDrawable {
